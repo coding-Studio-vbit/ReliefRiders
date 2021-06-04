c21abc0698aa953455143aa9c822a7eeeb88c611
bsaikiran618 <bsaikiran618@gmail.com>
2021-06-04 21:21:02 +0530

    Added mongoose connection and request OTP

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 8000;
+
+//express middleware usage.
+app.use(express.json());
+
+
+//mongoose connection.
+
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.xgkw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Import routers here.
 const authRouter = require("./routes/authentication/authRouter");
--- backend/routes/authentication/authRouter.js
+++ backend/routes/authentication/authRouter.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const riderAuthRouter = require("./riderAuth")
-const requesterAuthRouter = require("./requesterAuth")
 const otp = require("./OTP");
+const sms = require("../common/sms");
+const rider = require("../../models/rider");
+const requester = require("../../models/requester");
 
-router.use("/rider", riderAuthRouter);
-router.use("/requester", requesterAuthRouter);
+router.use("/requestOTP", (req, res)=>{
+	if(!req.body.type || !req.body.phone)
+		return res.status(400).json({err: "Invalid properties"});
+	
+	let userModel;
+
+	if(req.body.type == "rider")
+		userModel = rider;
+	else if(req.body.type == "requester")
+		userModel = requester;
+	else{
+		return res.status(400).json({err: "Invalid user type!"});
+	}
+
+
+	userModel.findOne({
+		phoneNumber: phone
+	})
+	.then(doc =>{
+		if(doc == null)
+			return res.json({err: "No such phone number! Please register."});
+
+		//phone number is valid.
+		//now create an otp and set it.
+		return otp.generateOTP();
+	})
+	.then(OTP=>{
+		fs.readFile("../OTP_Temp.json", (err, data)=>{
+			if(err){
+				console.log("An error occured while reading OTP_Temp.json", err);
+				return res.status(500).json({err: "Server internal error"});
+			}
+			else{
+				let obj = JSON.parse(data);
+				obj[phone] = {type: req.body.type, otp: OTP};
+				fs.writeFile("../OTP_Temp.json", JSON.stringify(obj), 'utf-8', (err)=>{
+					if(err){
+						console.log("An error occured while writing to OTP_Temp.json");
+						return res.status(500).json({err: "Server Internal Error"});
+					}
+					else{
+						console.log("New " + req.body.type + " login OTP request.");
+						sms.sendOTP(req.body.phone, OTP);
+					}
+				})
+			}
+		});
+	})
+	.catch(error=>{
+		console.log(error);
+		return res.status(500).json({err: "Server internal error"});
+	})
+})
 
 module.exports = router;
