35623073edb85e51a977476f1cf8c9d719b2bcfd
shivanishivalenka <shivalenkashivani@gmail.com>
2021-06-05 13:25:07 +0530

    add jwt verify

--- backend/routes/common/tokenAuth.js
+++ backend/routes/common/tokenAuth.js
@@ -1,8 +1,8 @@
 const JWT = require('jsonwebtoken')
 
 module.exports = {
-    verifyToken: (req,res,next) =>{
-        if(!req.headers['authorization']) return next(res.status(401))
+    verifyToken: (err,req,res,next) =>{
+        if(!req.headers['authorization']) return next(err)
         const authHeader = req.headers['authorization']
         const bearerToken = authHeader.split(' ')
         const token = bearerToken[1]
