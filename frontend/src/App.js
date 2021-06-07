8e42733b2e0fe5064456911c06cf05a20b036cfe
Anishahaha <itsaanishasingh@gmail.com>
2021-06-07 16:50:57 +0530

    Rider's Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter as Router} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import RiderHome from "./components/rider/RiderHome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Router>
+        <Switch>
+        <Route path="/" component={RiderHome} />
+        </Switch>
+      </Router>
+     {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
     </div>
   );
 }
