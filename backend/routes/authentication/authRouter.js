db8aabb7f67b9bea1038c0df618498dbf7583983
Sai Kiran Boddu <bsaikiran618@gmail.com>
2021-06-07 19:26:50 +0530

    Updated endpoints for login and added registration endpoint for request OTP

--- backend/routes/authentication/authRouter.js
+++ backend/routes/authentication/authRouter.js
@@ -12,7 +12,7 @@ const OTP_FILE_PATH = "./TEMP_OTP.json";
 const OTP_TIMEOUT_MINS = 5;
 
 
-router.post("/requestOTP", (req, res)=>{
+router.post("/login/requestOTP", (req, res)=>{
 
 	if(!req.body.type || !req.body.phone)
 		return res.json({status:"failure", message: "Invalid properties"});
@@ -96,20 +96,11 @@ router.post("/requestOTP", (req, res)=>{
 
 router.post("/verifyOTP", (req, res)=>{
 
-	if(!req.body.phone || !req.body.type || !req.body.OTP){
+	if(!req.body.phone ||  !req.body.OTP){
 		return res.json({status:"failure", message: "Invalid properties"});
 	}
 
-	let {phone, type, OTP} = req.body;
-	let userModel;
-	switch(type)
-	{
-		case "rider": userModel = rider; break;
-		case "requester":userModel = requester;break;
-		default: userModel = null;
-	}
-	if(!userModel)
-		return res.json({status:"failure", message: "Invalid user type!"});
+	let {phone, OTP} = req.body;
 	
 	fs.readFile(OTP_FILE_PATH, (err, data)=>{
 		if(err)
@@ -122,11 +113,11 @@ router.post("/verifyOTP", (req, res)=>{
 		{
 			const tempData = JSON.parse(JSON.stringify(obj));
 
-			if(tempData[phone].type == type && tempData[phone].OTP == OTP)
+			if(tempData[phone].OTP == OTP)
 			{
 				var token = jwt.sign({
 					phoneNumber: phone,
-					userType: type
+					userType: tempData[phone].type
 				}, process.env.TOKEN_SECRET);
 				
 				return res.json({status:"success", message: token});
@@ -144,5 +135,87 @@ router.post("/verifyOTP", (req, res)=>{
 	})
 	
 })
+
+
+router.post("/register/requestOTP", (req, res)=>{
+
+	if(!req.body.phone || !req.body.type)
+	{
+		return res.json({status: "failure", message: "Invalid properties."});
+	}
+	
+	let userModel;
+
+	const {type, phone} = req.body;
+
+	switch(type)
+	{
+		case "rider": userModel = rider; break;
+		case "requester":userModel = requester;break;
+		default: userModel = null;
+	}
+	if(!userModel)
+		return res.json({status:"failure", message: "Invalid user type!"});
+
+
+	userModel.findOne({
+		phoneNumber: phone
+	})
+	.then(doc=>{
+		if(doc != null)
+		{
+			throw {status: "failure", message:"User already exists."}
+		}
+		return otp.generateOTP();
+	})
+	.then(OTP=>{
+		
+		fs.readFile(OTP_FILE_PATH, (err, data)=>{
+			if(err){
+				console.log("An error occured while reading ", OTP_FILE_PATH, err);
+				throw ({status: "failure", message: "Server internal error"});
+			}
+			else{
+				let obj = JSON.parse(data);
+				obj[phone] = {type: req.body.type, otp: OTP};
+				fs.writeFile(OTP_FILE_PATH, JSON.stringify(obj), 'utf-8', (err)=>{
+					if(err){
+						console.log("An error occured while writing to OTP_Temp.json");
+						throw({status:"failure", message: "Server Internal Error"});
+					}
+					else{
+
+						//delete this OTP after 5 mins.
+						
+						setTimeout(()=>{
+							fs.readFile(OTP_FILE_PATH, (err, data)=>{
+								if(err){
+									console.log("An error occured while trying to read and delete OTP from ", OTP_FILE_PATH, err);
+								}
+								let obj = JSON.parse(data);
+								delete obj[phone];
+								fs.writeFile(OTP_FILE_PATH, JSON.stringify(obj), 'utf-8', err=>{
+									if(err){
+										console.log("An error occured while writing to ", OTP_FILE_PATH, err);
+									}
+								})
+							})
+						}, OTP_TIMEOUT_MINS * 60 * 1000);
+
+						// So after 5 mins, the OTP entry will not exist in the file.
+
+						console.log("New " + req.body.type + " registration OTP request.");
+						res.json({status: "success", message:"OTP Set"});
+						sms.sendOTP(req.body.phone, OTP);
+					}
+				})
+			}
+		});
+	})
+	.catch(error=>{
+		console.log(error)
+		res.json(error)
+	})
+});
 
 module.exports = router;
