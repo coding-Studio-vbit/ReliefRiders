625c39da7ad6e2afc86a0b9a6f2f8910d3fb39ab
Poojithamutteneni <tgb.thegirlboss@gmail.com>
2021-06-08 22:24:37 +0530

    Officially Profile Page rider/requester update. We're Over!

--- backend/routes/requester/requesterProfile.js
+++ backend/routes/requester/requesterProfile.js
@@ -25,7 +25,7 @@ router.get("/requester/profile", verifyToken,  function(req, res)  {
 
 
 router.put("/requester/updateProfile", verifyToken, function(req,res) {
-  requester.findOneAndUpdate({phoneNumber: req.user.phoneNumber }, {$set:{name:req.body.name, yearOfBirth: req.body.yearOfBirth, defaultAddress: {addressLine: req.body.addressLine, city: req.body.city, pincode: req.body.pincode}}}, {new: true}, function(err, doc) {
+  requester.findOneAndUpdate({phoneNumber: req.user.phoneNumber }, {$set:{name:req.body.name, yearOfBirth: req.body.yearOfBirth, 'defaultAddress.addressLine': req.body.defaultAddress.addressLine, 'defaultAddress.city': req.body.defaultAddress.city, 'defaultAddress.pincode': req.body.defaultAddress.pincode}}, {new: true}, function(err, doc) {
     if (err) {
       res.json({
       status:"failure",
