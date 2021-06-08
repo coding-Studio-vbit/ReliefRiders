78135c53531d09c15399592a80a5ae26bb3b6cf2
Poojithamutteneni <tgb.thegirlboss@gmail.com>
2021-06-08 22:04:24 +0530

    update requester's profile backend

--- backend/routes/requester/requesterProfile.js
+++ backend/routes/requester/requesterProfile.js
@@ -16,12 +16,30 @@ router.get("/requester/profile", verifyToken,  function(req, res)  {
   else {
   res.json({
     status:"success",
-    message: "Requesters profile",
+    message: "Requester's profile",
     result : result
   })
  }
 })
 })
 
+
+router.put("/requester/updateProfile", verifyToken, function(req,res) {
+  requester.findOneAndUpdate({phoneNumber: req.user.phoneNumber }, {$set:{name:req.body.name, yearOfBirth: req.body.yearOfBirth, defaultAddress: {addressLine: req.body.addressLine, city: req.body.city, pincode: req.body.pincode}}}, {new: true}, function(err, doc) {
+    if (err) {
+      res.json({
+      status:"failure",
+      message: "Unable to retrieve data"
+  })
+     console.error(err);
+    }
+    else {
+    res.json({
+      status:"success",
+      message: "Requester's profile updated"
+    })
+   }
+  })
+})
 
 module.exports = router;
--- backend/routes/rider/riderProfile.js
+++ backend/routes/rider/riderProfile.js
@@ -23,4 +23,23 @@ router.get("/rider/profile", verifyToken,  function(req, res)  {
 })
 
 
+router.put("/rider/updateProfile", verifyToken, function(req,res) {
+  rider.findOneAndUpdate({phoneNumber: req.user.phoneNumber }, {$set:{name:req.body.name}}, {new: true}, function(err, doc) {
+    if (err) {
+      res.json({
+      status:"failure",
+      message: "Unable to retrieve data"
+  })
+     console.error(err);
+    }
+    else {
+    res.json({
+      status:"success",
+      message: "Rider's profile updated"
+    })
+   }
+  })
+})
+
+
 module.exports = router;
