58a76d4ce007ef0bcc13f39797e7d73f9f48aaa8
Rev <rev@pop-os.localdomain>
2021-06-08 09:45:05 +0530

    Add initial router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './App.css'
-import Login from './components/authentication/ui/login/login';
+// import Login from './components/authentication/ui/login/login';
 import { AuthProvider } from './components/context/auth/authProvider';
 // import VerifyOTP from './components/authentication/ui/otp/verify_otp';
 // import RegisterScreen from './components/authentication/ui/register/register'
 // import Spinner from './components/global_ui/spinner';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Spinner radius="2" /> */}
-      {/* <VerifyOTP/> */}
-      <AuthProvider>
-      <Login/>
-      </AuthProvider>
-      {/* <RegisterScreen /> */}
+      <Router>
+        <Switch>
+          <Route path="/about">
+            About
+          </Route>
+          <Route path="/">
+            <AuthProvider>
+              <p>Home</p>
+            </AuthProvider>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
