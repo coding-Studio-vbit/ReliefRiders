e9624a60f05b45a9f6987df69db423fe9cefe2ff
Rev <rev@pop-os.localdomain>
2021-06-09 09:37:13 +0530

    Router testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,27 @@ import React from 'react';
 import './App.css'
 import Login from './components/authentication/ui/login/login';
 import { AuthProvider } from './components/context/auth/authProvider';
-// import VerifyOTP from './components/authentication/ui/otp/verify_otp';
-// import RegisterScreen from './components/authentication/ui/register/register'
-// import Spinner from './components/global_ui/spinner';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
+import RegisterScreen from './components/authentication/ui/register/register'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Switch>
           <Route path="/about">
             About
           </Route>
           <Route path="/">
             <AuthProvider>
-              <RegisterScreen/>
+            
             </AuthProvider>
           </Route>
         </Switch>
-      </Router> */}
+      </Router>
 
       <AuthProvider>
               <Login/>
