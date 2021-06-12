9ced39fb015b906544a53fc84830c551022fade5
Anishahaha <itsaanishasingh@gmail.com>
2021-06-12 21:47:33 +0530

    Changed file location

--- frontend/src/components/home/Rider/RiderHome.js
+++ frontend/src/components/home/Rider/RiderHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useHistory} from 'react-router-dom';
-import Logo from "../global_ui/logo";
+import Logo from "./../../global_ui/logo";
 import "./RiderHome.css";
 function RiderHome() {   
     const history =useHistory()
--- frontend/src/components/home/initial_home/initialHomeRouting.js
+++ frontend/src/components/home/initial_home/initialHomeRouting.js
@@ -9,7 +9,7 @@ import Login from "../../authentication/ui/login/login";
 import VerifyOTP from "../../authentication/ui/otp/verify_otp";
 import RegisterScreen from "../../authentication/ui/register/register_form";
 import { AuthContext } from "../../context/auth/authProvider";
-import RiderHome from "../../rider/RiderHome";
+import RiderHome from "../Rider/RiderHome";
 import RequesterHomeScreen from "../Requester/RequesterHomeScreen";
 import InitialHome from "./initial_home";
 const InitialHomeRouting = () => {
