64c3189afa1b3e6fb6e05fef3a18de117288cc8f
Rev <rev@pop-os.localdomain>
2021-06-14 14:17:02 +0530

    Style placed_requests

--- frontend/src/components/global_ui/nav.js
+++ frontend/src/components/global_ui/nav.js
@@ -2,13 +2,14 @@ import React from 'react';
 const Navbar = ({title}) => {
     return ( 
         <nav 
-        
+        className="nav-bar"
         style={{
             width:'100%',
+            padding: 0.7+'em',
             height:'fit-content',
             textAlign:'center',
             letterSpacing:2+'px',
-            fontSize:1.3+'em',
+            fontSize:1.1+'em',
             color:'black'
         }}>
             <h3>
--- frontend/src/components/requester/my_requests/placed_request.js
+++ frontend/src/components/requester/my_requests/placed_request.js
@@ -1,54 +1,173 @@
-import React from 'react';
-import { useLocation } from 'react-router';
-
+import React from "react";
+// import { useLocation } from 'react-router';
+import Navbar from "../../global_ui/nav";
+import styles from "./placed_request.module.css";
 const PlacedRequest = () => {
+  // const {state:{request}} = useLocation()
+  const request = {
+    requesterID: "8628290",
+    requestStatus: "PENDING",
+    requestType: "P&D",
+    itemsListImages: [
+      // "https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    ],
+    itemsListList: [
+      {
+        itemName: "Tomato",
+        quantity: "2kg",
+      },
+      {
+        itemName: "Tomato",
+        quantity: "2kg",
+      },
+    ],
+    itemCategories: ["MEDICINES", "MISC"],
+    remarks: "Something here",
+    billsImageList: [
+      "https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    ],
+    rideImages: [
+      "https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    ],
+    pickupLocationAddress: {
+      addressLine: "Some place far away",
+      area: "",
+      city: "Unknown",
+      pincode: "XXXXXX",
+    },
+    dropLocationAddress: {
+      addressLine: "Some place far away",
+      area: "",
+      city: "Unknown",
+      pincode: "XXXXXX",
+    },
+  };
+  const statusStyle = {
+    color: request.requestStatus === "PENDING" ? "red" : "green",
+    fontWeight: "bold",
+    fontSize: 1.2 + "em",
+  };
+  //quantity  -- must be string
+  //itemsListType  -- unnecessary
+  //RiderName in screen how if order is delivered?
+  //remarks -- lowercase the remarks
+  //Extra area property in address ? what?
+  return (
+    <>
+      <Navbar title="Order Details" />
+      <div className={styles.container}>
+        <p>Request #{request.requesterID}</p>
+        <span>
+          Order Status:{" "}
+          <span style={statusStyle}> {request.requestStatus}</span>
+        </span>
+        {request.requestStatus[0] === "D" && <p>Order deliver</p>}
+        <Address
+          pickup={request.pickupLocationAddress}
+          drop={request.dropLocationAddress}
+          type={request.requestType}
+        />
+        {request.itemsListImages.length > 0 ? (
+          <ItemsRequestedImagesAndOthers
+            bills={request.billsImageList}
+            items={request.itemsListImages}
+            images = {request.rideImages}
+          />
+        ) : (
+          <>
+          
+          <ItemsRequestedList
+            list={request.itemsListList}
+            category={request.itemCategories}
+          />
+          <ItemsRequestedImagesAndOthers
+            bills={request.billsImageList}
+            images = {request.rideImages}
+          />
+          </>
+        )}
+        
+      </div>
+    </>
+  );
+};
 
-    // const {state:{request}} = useLocation()
-    const request = {
-        requesterID:"8628290",
-        requestStatus:"PENDING",
-        requestType:"P&D",
-        itemsListImages:[
-            "imageshere"
-        ],
-        itemsListList:[
-            {
-                itemName:"Tomato",
-                quantity:"2kg"
-            }
-        ],
-        itemCategories:[
-            
-                "MEDICINES",
-                "MISC"
-            
-        ],
-        remarks: "Something here",
-        billsImageList:["imageshere"],
-        rideImages:["imgs"],
-        pickupLocationAddress:{
-            addressLine:"",
-            area:"",
-            city:"",
-            pincode:""
-
-        },
-        dropLocationAddress:{
-            addressLine:"",
-            area:"",
-            city:"",
-            pincode:""
-
-        },
-    }
-    //quantity 
-    //itemsListType
-    
-    return ( 
-        <div>
-
-        </div>
-    )
-}
- 
 export default PlacedRequest;
+
+const ItemsRequestedImagesAndOthers = ({bills, images ,items=[]}) => {
+  return (
+    <div className={styles.imagesContainer}>
+    {items.map((link) => (
+        <div className={styles.singleImage} key={link}>
+          <img src={link} alt="items-img" />
+          <span>Items</span>
+        </div>
+      ))}
+      {bills.map((link,index) => (
+        <div className={styles.singleImage} key={link}>
+          <img src={link} alt="items-img" />
+          <span>Bill #{index+1}</span>
+        </div>
+      ))}
+      {images.map((link) => (
+        <div className={styles.singleImage} key={link}>
+          <img src={link} alt="items-img" />
+          <span>Rider-Selfie</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const ItemsRequestedList = ({ list, category }) => {
+  return (
+    <div className={styles.requested}>
+      <span className={styles.irItems}>Items Requested</span>
+
+      <div className={styles.category}>
+        {category.map((cat) => (
+          <span
+            className={cat[1] === "E" ? styles.catGreen : styles.catGray}
+            key={cat}
+          >
+            {cat}
+          </span>
+        ))}
+      </div>
+      <div className={styles.items}>
+        {list.map((item) => (
+          <span key={item.itemName}>
+            {item.itemName} - {item.quantity}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Address = ({ type, pickup, drop }) => {
+  return (
+    <div className={styles.addressContainer}>
+      {type === "P&D" && <span>Pickup Location</span>}
+      <div className={styles.address}>
+        <span>Address</span>
+        <span>{pickup.addressLine}</span>
+        <span>
+          {pickup.city} {pickup.pincode}
+        </span>
+      </div>
+      {drop && (
+        <>
+          <span>Drop Location</span>
+          <div className={styles.address}>
+            <span>Address</span>
+            <span>{pickup.addressLine}</span>
+            <span>
+              {pickup.city} {pickup.pincode}
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
