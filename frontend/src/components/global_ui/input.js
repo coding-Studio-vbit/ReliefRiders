94511311105171999c9177539e3bba1af69ff8e9
prashanith007 <prashanith007@gmail.com>
2021-06-17 16:33:15 +0530

    Global UI changes INPUT

--- frontend/src/components/global_ui/input.js
+++ frontend/src/components/global_ui/input.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import './input.css'
 
-
 /**
  * 
  * @param {*} props textAlign,size,minLength,value,placeholder,name,type,onChange,error,maxLength are the supported props
  * You can pass an error msg to show an error below input field
  * @returns a custom styled input component.
  */
-const InputField = ({textAlign,size,minLength,value,placeholder,name,type,onChange,error,maxLength}) => {
-
+const InputField = ({
+    textAreaClass,rows,cols,fieldType,
+    textAlign,size,minLength,value,placeholder,name,type,onChange,error,maxLength}) => {
+    
     return ( 
-
         <div className="input-field">
-            <input size={size} style={{textAlign:textAlign}} className={error?"error-field":"normal-field"} type={type} name={name} value={value} minLength={minLength} maxLength={maxLength} onChange={onChange} required/>
-            <label className="label" htmlFor={name}>{placeholder}</label>
+            {
+                fieldType=="textarea" &&
+                <textarea
+                rows={rows}
+                cols={cols}
+                 size={size} style={{textAlign:textAlign}} className={`${error?"error-field":"normal-field"} ${textAreaClass}`} type={type} name={name} value={value} minLength={minLength} maxLength={maxLength} onChange={onChange} required/>
+            }
+            {
+                fieldType=="textarea" &&                
+                <label className="label" htmlFor={name}>{placeholder}</label>
+            }            
+            {
+                !fieldType &&
+                <input 
+                size={size} 
+                style={{textAlign:textAlign}} 
+                className={error?"error-field":"normal-field"} 
+                type={type} name={name} value={value} minLength={minLength} maxLength={maxLength} onChange={onChange} required/>
+            }
+            {
+                !fieldType &&
+                <label className="label" htmlFor={name}>{placeholder}</label>
+            }            
             <br />
+            
             {error && <span className="error-msg" >{error}</span>}
             {error && <div style={{ height: 1 + 'rem' }} ></div>}
 
