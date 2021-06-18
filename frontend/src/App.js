cc1d56060a1efd5f120ea6963eceb88ab70903d5
prashanith007 <prashanith007@gmail.com>
2021-06-18 22:15:05 +0530

    Global UI buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import { AuthProvider } from './components/context/auth/authProvider';
 import {
@@ -8,13 +8,22 @@ import {
 } from "react-router-dom";
 import InitialHomeRouting from './components/home/initial_home/initialHomeRouting';
 import PlacedRequest from './components/requester/my_requests/placed_request';
+import Button from './components/global_ui/buttons/button';
+
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleOnChange = () => {
+    console.log("fjfkfkfjk");
+    setIsChecked(!isChecked);
+  };
+
   //todo
 
   return (
     <div className="App">
-      <AuthProvider>
+      {/* <AuthProvider>
         <Router>
           <Switch>
           
@@ -27,7 +36,32 @@ function App() {
           </Switch>
         </Router>
 
-      </AuthProvider>
+      </AuthProvider> */}
+      <Button 
+      text="SUBMIT" bgColor="red" color="white"
+      isRounded="true"
+      paddingTB="20px"
+      paddingLR="17px"
+      borderWidth="0px"
+      isBlock="true"
+      onClick={ ()=>console.log("Clicked")}
+        />
+
+        <span className="bc">
+       
+      <label htmlFor="topping">ckck</label>
+      <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleOnChange}
+        />
+
+        </span>
+
+     
+        
+      
+      
     </div>
   );
 }
