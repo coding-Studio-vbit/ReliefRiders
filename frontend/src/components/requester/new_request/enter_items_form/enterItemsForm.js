17063122d863719742252187d078231b071081a9
aaris03khan <aaris03khan@gmail.com>
2021-06-18 20:16:50 +0530

    Requester - Enter Items Screen added

--- frontend/src/components/requester/new_request/enter_items_form/enterItemsForm.js
+++ frontend/src/components/requester/new_request/enter_items_form/enterItemsForm.js
@@ -1,6 +1,9 @@
+//React
 import React, { useState, useEffect } from 'react';
 
-import TopNav from './top-nav';
+//CSS
+import InputField from "../../../global_ui/input";
+import Navbar from "../../global_ui/nav";
 import styles from './requestItem.module.css'
 import 'font-awesome/css/font-awesome.min.css';
 
@@ -40,7 +43,7 @@ function EnterItemsForm() {
 		<div className={'container'}>
  		{/* Navbar */}
  		<div className={'row'}>
- 			<TopNav />
+			<Navbar style={{background:"palegreen",marginBottom:0.75+'em'}}  title="Enter Items" back/>
  		</div>
 
  		{/* Prompt Text */}
@@ -77,13 +80,13 @@ function EnterItemsForm() {
 				return (
 					<div className={'row'} style={{marginTop: '2%'}}>
 						<div className={'col'}>
-							<input className="form-control" type="text" name="itemName" value={x.itemName} onChange={e => handleInputChange(e, i)} placeholder="Item Name..."></input>
+							<InputField type="text" placeholder="Item Name..." name="itemName" value={x.itemName} onChange={e => handleInputChange(e, i)}/>
 						</div>
 						<div className={'col'}>
-							<input className="form-control" type="number" name="itemQty" value={x.itemQty} onChange={e => handleInputChange(e, i)} placeholder="Item qty..."></input>
+							<InputField type="number" placeholder="Item qty..." name="itemQty" value={x.itemQty} onChange={e => handleInputChange(e, i)}/>
 						</div>
 						<div className={'col'}>
-							{inputList.length - 1 === i && <button style={{marginRight: '2%'}} type="button" className="btn btn-success" onClick={handleAddClick} value="Add">Add</button>}
+							{inputList.length - 1 === i && <button style={{marginRight: '2%', alignContent: 'center'}} type="button" className="btn btn-success" onClick={handleAddClick} value="Add">Add</button>}
 							{inputList.length !== 1 && <button style={{marginLeft: '1%'}} type="button" className="btn btn-danger" onClick={() => handleRemoveClick(i)} >X</button>}
 						</div>
  					</div>
@@ -92,16 +95,19 @@ function EnterItemsForm() {
 
 			<div className={'row'} style={{marginTop: '5%'}}>
 				<div className={'col'}>
-					<input style={{backgroundColor: 'red'}} class="form-check-input" type="checkbox" value="" id="flexCheckDisabled"></input>
-  					<label style={{marginLeft: '5px'}} class="form-check-label" for="flexCheckDisabled">Medicine</label>
+					<input style={{backgroundColor: 'red'}} className="form-check-input" type="checkbox" value="" id="flexCheckDisabled"></input>
+  					{/* <label style={{marginLeft: '5px'}} className="form-check-label" for="flexCheckDisabled">Medicine</label> */}
+					<p>Medicine</p>
 				</div>
 				<div className={'col'}>
 					<input style={{backgroundColor: 'green'}} class="form-check-input" type="checkbox" value="" id="flexCheckDisabled"></input>
-  					<label style={{marginLeft: '5px'}} class="form-check-label" for="flexCheckDisabled">Grocery</label>
+  					{/* <label style={{marginLeft: '5px'}} class="form-check-label" for="flexCheckDisabled">Grocery</label> */}
+					<p>Grocery</p>
 				</div>
 				<div className={'col'}>
 					<input style={{backgroundColor: 'blue'}} class="form-check-input" type="checkbox" value="" id="flexCheckDisabled"></input>
-  					<label style={{marginLeft: '5px'}} class="form-check-label" for="flexCheckDisabled">Misc</label>
+  					{/* <label style={{marginLeft: '5px'}} class="form-check-label" for="flexCheckDisabled">Misc</label> */}
+					<p>Misc</p>
 				</div>
 			</div>
 
