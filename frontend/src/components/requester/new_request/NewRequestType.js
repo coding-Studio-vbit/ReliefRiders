8c533215057b12d5a45f23b7f69173365c5fe3c2
chandrakiran343 <80049363+chandrakiran343@users.noreply.github.com>
2021-06-19 10:32:07 +0530

    added css module for New Request Type screen

--- frontend/src/components/requester/new_request/NewRequestType.js
+++ frontend/src/components/requester/new_request/NewRequestType.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import Logo from '../../global_ui/logo'
-import './RequestType.css'
+import Requests from './RequestType.module.css'
 
 function RequestType() {
     return(
       <div>
-        <nav className='navbar'>
-          <div className='back_div'>
-            <button className='btn_back'>
-              <span className='i1'>
+        <nav className={Requests.navbar}>
+          <div className={Requests.back_div}>
+            <button className={Requests.btn_back}>
+              <span className={Requests.i1}>
               <i className='fas fa-chevron-left'></i>
               </span>
             </button>
           </div>
-          <span className='nav_head'>
+          <span className={Requests.nav_head}>
               Choose Request Type
             </span>
         </nav>
-        <div className='container'>
+        <div className={Requests.container}>
           <Logo />
-          <button className='btn2'>
-            <span className='i2'>
-            <span className='box'>
+          <button className={Requests.btn2}>
+            <span className={Requests.i2}>
+            <span className={Requests.box}>
               <i className='fas fa-plus'></i>
             </span>
             </span>
-            <span className='btn2_text'>
+            <span className={Requests.btn2_text}>
               General Request
             </span>
           </button>
-          <button className='btn2'>
-            <span className='icon2'>
-              <span className='i3'>
+          <button className={Requests.btn2}>
+            <span className={Requests.icon2}>
+              <span className={Requests.i3}>
               <i className='fas fa-truck-pickup'></i>
               </span>
             </span>
-            <span className='btn2_text'>
+            <span className={Requests.btn2_text}>
               Pick Up / Drop
             </span>
           </button>
