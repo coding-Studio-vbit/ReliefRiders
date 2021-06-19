b67fe844335fb88a938576916cc4f2ea3162db8d
Anishahaha <itsaanishasingh@gmail.com>
2021-06-19 20:59:50 +0530

    Added CSS Module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,8 @@ import {
 } from "react-router-dom";
 import InitialHomeRouting from './components/home/initial_home/initialHomeRouting';
 import PlacedRequest from './components/requester/my_requests/placed_request';
-//import PinAddress from './components/requester/new_request/PinAddress';
+import PinAddress from './components/requester/new_request/PinAddress';
+//import MapComponent from './components/requester/new_request/MapComponent';
 function App() {
   //todo
 
@@ -17,8 +18,11 @@ function App() {
       <AuthProvider>
         <Router>
           <Switch>
-          {/* <Route path="/">
+           <Route path="/address">
               <PinAddress />
+            </Route>
+            {/* <Route path="/address">
+              <MapComponent />
             </Route> */}
             <Route path="/about">
               <PlacedRequest />
