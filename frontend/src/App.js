7961bd8ed6e6ec5a83ae8af1c6500c5c7c68038e
prashanith007 <prashanith007@gmail.com>
2021-06-21 21:03:32 +0530

    TextArea Global UI Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,31 @@ import {
   Route,
 } from "react-router-dom";
 import InitialHomeRouting from './components/home/initial_home/initialHomeRouting';
+import PlacedRequest from './components/requester/my_requests/placed_request';
+
+
 function App() {
+  //todo
 
   return (
     <div className="App">
+      {/* <Map/> */}
+      {/* <MyMapComponent/> */}
+    
       <AuthProvider>
         <Router>
           <Switch>
-            <Route path="/temp">
-              TestYourScreensHere
-            </Route>
+          
+            <Route path="/about">
+              <PlacedRequest/>
+          </Route>
             <Route path="/">
               <InitialHomeRouting />
             </Route>
           </Switch>
         </Router>
 
-      </AuthProvider>       
+      </AuthProvider>
     </div>
   );
 }
--- frontend/src/components/global_ui/input.js
+++ frontend/src/components/global_ui/input.js
@@ -7,22 +7,16 @@ import styles from './input.module.css'
  * You can pass an error msg to show an error below input field
  * @returns a custom styled input component.
  */
-const InputField = ({
-    textAreaClass,rows,cols,fieldType,
-    textAlign,size,minLength,value,placeholder,name,type,onChange,error,maxLength}) => {
+const InputField = ({textAlign,size,minLength,value,placeholder,name,type,onChange,error,maxLength}) => {
     
     return ( 
-
         <div className={styles.inputField}>
             <input size={size} style={{textAlign:textAlign}} className={styles.input+" "+(error?styles.errorField:styles.normalField)} type={type} name={name} value={value} minLength={minLength} maxLength={maxLength} onChange={onChange} required/>
             <label className={styles.label} htmlFor={name}>{placeholder}</label>
             <br />
             {error && <span className={styles.errorMsg} >{error}</span>}
             {error && <div style={{ height: 1 + 'rem' }} ></div>}
-
         </div>
-
-
      );
 }
  
