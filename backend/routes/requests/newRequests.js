4a30489790fd089978b68baa67e2f94f185eada5
Siddharth Malladi <39798296+SidMalladi@users.noreply.github.com>
2021-06-27 15:26:33 +0530

    Add files via upload

--- backend/routes/requests/newRequests.js
+++ backend/routes/requests/newRequests.js
@@ -1,88 +1,97 @@
 const express = require('express');
 const router  = express.Router();
-const dotenv = require('dotenv');
 const requestModel = require('../../models/request');
-const verifyToken = require('../common/tokenAuth');
-//dependencies and packages (multer, cors)
+const requester = require('../../models/requesters')
 const multer = require("multer");
-var md5 = require('md5');
-const fs = require('fs')
+const md5 = require('md5');
+const fs = require('fs');
 const path = require('path');
+const verifyToken = require('../common/tokenAuth');
 
 router.use(verifyToken);
 
-dotenv.config()
 
 
-//Storing files in the server using Multer
-var paths = [];
+//multer storage
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         let hashValue = md5(file.originalname+Date.now());
-        pathValue = path.join('data\images',hashValue.slice(0,1),hashValue.slice(0,2))
-        if (fs.existsSync(pathValue)) {
-            cb(null, (pathValue))
-          }
-        else{
-            fs.mkdir(pathValue,{ recursive: true },(err)=>{
-                if (err) { console.log('Cannot store files! Path Error!')}
-                else{
-                    cb(null, (pathValue))
-                }
-            })
+        pathValue = path.join(__dirname, '../../../data/images', hashValue.slice(0,1),hashValue.slice(0,2))
+
+        var stat = null;
+        try {
+            stat = fs.statSync(pathValue);
+        } catch (err) {
+				fs.mkdirSync(pathValue, {recursive: true});
         }
+        if (stat && !stat.isDirectory()) {
+            throw new Error('Directory cannot be created because an inode of a different type exists at "' + pathValue + '"');
+        }       
+        cb(null, pathValue);
+     
     },
     filename: function (req, file, cb) {
-        fileName = file.fieldname+Date.now() + file.originalname
-        cb(null, fileName)
-        paths.push(path.join(pathValue,fileName))
-        console.log(paths)
+		console.log(file);
+        fileName = md5(file.fieldname + String(Date.now()) + file.originalname )+ "." + file.mimetype.slice(6);
+        return cb(null, fileName)
     }
 });
 
 var upload = multer({ storage: storage })
 
 
-var pastReqTime = 0
-router.post('/requests/newRequest/pd',upload.any('images'),(req,res)=>{
-    let newReqSetTime = Date.now()
-    if (newReqSetTime-pastReqTime <= process.env.REQUEST_INTERVAL){
-        return res.json({status: "failure", message: "Files failed to be uploaded"})
-    }
-    let paths = [];
-    try{
-        req.files.map(data=>{
-            paths.push(data.path)
-        })
-    }
-    catch(err){
-        console.log("Files cannot be fetched!")
-    }
-    let newRequest = new requestModel({
-        requestNumber : Date.now() + Math.floor(Math.random()*100),
-        requesterCovidStatus : req.body.requesterCovidStatus,
-        requestStatus : req.body.requestStatus,
-        itemsListList: req.body.itemsListList,
-        itemCategories : req.body.itemCategories,
-        Remarks: req.body.Remarks,
-        billsImageList : paths,
-        rideImages: req.body.rideImages,
-        pickupLocationCoordinates: req.body.pickupLocationCoordinates,
-	    pickupLocationAddress: req.body.pickupLocationAddress,
-        dropLocationCoordinates: req.body.dropLocationCoordinates,
-        dropLocationAddress: req.body.dropLocationAddress
-    }); 
-    newRequest.save()
+router.post('/newRequest/pd',upload.any('images'),(req,res)=>{
+    let currentReqTime = Date.now();
+    console.log(currentReqTime);
+    requester.findOne({phoneNumber : req.user.phoneNumber})
+    .then(doc => {
+        if(doc != null)
+        {
+          //Fetch last request time here
+        }
+        return 16273927;
+    })
+    .then(value =>{
+        let paths = [];
+        
+        if (req.files){
+            req.files.map(data=>{
+				var path = data.path;
+				var path2 = "data/images";
+                paths.push(path.slice(path.search(path2) + path2.length));
+            })
+        }
+        console.log(paths)
+        return paths 
+    })
+    .then(paths =>{
+        console.log(req.body);
+        let newRequest = new requestModel({
+            requestNumber : Date.now() + Math.floor(Math.random()*100),
+            requesterCovidStatus : req.body.requesterCovidStatus,
+            noContactDelivery : req.body.noContactDelivery, // Added no contact delivery
+            requestStatus : req.body.requestStatus,
+            requestType : 'P&D',
+            itemsListImages : paths,
+            itemsListList : JSON.parse(req.body.itemsListList),
+            itemCategories : req.body.itemCategories,
+            Remarks: req.body.Remarks,
+			dropLocationCoordinates: {coordinates : req.body.dropLocationCoordinates},
+            dropLocationAddress: req.body.dropLocationAddress,
+            pickupLocationCoordinates: {coordinates : req.body.pickupLocationCoordinates},
+            pickupLocationAddress: req.body.pickupLocationAddress,
+
+        }); 
+        return newRequest.save()    
+    })
     .then(result =>{
-        return res.json({status:"success!", message: "Request accepted successfully"})
+        return res.json({status:"success", message: "Request successfully made"})
     })
     .catch(err =>{
-        return res.json({status:"failure!",message:"Sorry! Request could not be accepted"})
-    })
-
-    pastReqTime = newReqSetTime;
-    
-    
-    
+			console.log(err);
+            return res.json()
+    })    
 })
+// --------------------------
 module.exports = router;
