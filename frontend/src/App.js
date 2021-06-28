7d1d8b70f2bfb1ab4b478cb484b387ee34dd9986
prashanith <prashanith007@gmail.com>
2021-06-28 15:34:02 +0530

    Global UI fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css'
-import { AuthProvider } from './components/context/auth/authProvider';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import InitialHomeRouting from './components/home/initial_home/initialHomeRouting';
+// import { AuthProvider } from './components/context/auth/authProvider';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
+// import InitialHomeRouting from './components/home/initial_home/initialHomeRouting';
+import Dialog from './components/global_ui/nav'
 
 function App() {
   return (
-    <div className="App">    
-      <AuthProvider>
+    <div className="App">
+    <Dialog style={{background:"red"}}/>   
+      {/* <AuthProvider>
         <Router>
           <Switch>          
             <Route path="/about">
@@ -22,7 +24,7 @@ function App() {
             </Route>
           </Switch>
         </Router>
-      </AuthProvider>
+      </AuthProvider> */}
     </div>
   );
 }
