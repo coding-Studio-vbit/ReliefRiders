280b6ba5db64640e330df919155981341e2586ba
chandrakiran343 <80049363+chandrakiran343@users.noreply.github.com>
2021-07-02 15:01:36 +0530

    now responsive

--- frontend/src/components/requester/new_request/NewRequestType.js
+++ frontend/src/components/requester/new_request/NewRequestType.js
@@ -13,12 +13,14 @@ function RequestType() {
   };
   
     return(
-      <div>
+      <div style={{display:'grid'}}>
         <div className={'row'}>
-			    <Navbar back='/requester' backStyle={{ color: "white" }} title="Choose Request Type" titleStyle={{ color: "white" }} style={{ backgroundColor: "#79CBC5", marginBottom: "10px" }}/>
+			    <Navbar back='/requester' backStyle={{ color: "white", position:'absolute',left:0}} title="Choose Request Type" titleStyle={{ color: "white",fontSize:'18px'}} style={{ backgroundColor: "#79CBC5", marginBottom: "10px", display:'flex', position:'relative' }}/>
  		    </div>
         <div className={rtstyles.rcontainer}>
-          <Logo />
+        <span className={rtstyles.rimg}>
+            <Logo />
+            </span>
           <button onClick={() => routehandler("list_type")} className={rtstyles.rbtn2}>
             <span className={rtstyles.ri2}>
             <span className={rtstyles.rbox}>
