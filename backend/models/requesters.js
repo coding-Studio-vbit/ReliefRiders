ef238d6f1cc1f1038014c7ac1f4d9250c83e464d
Sai Kiran Boddu <bsaikiran618@gmail.com>
2021-07-07 22:05:34 +0530

    update

--- backend/models/requesters.js
+++ backend/models/requesters.js
@@ -8,48 +8,48 @@ const user = new mongoose.Schema({
     required: [true, "Phone Number is required"],
     min: 1000000000,
     max: 9999999999,
-    validate:{
-          validator: (phone)=> {
-            var patt = /^[6789]\d{9}$/; return patt.test(phone)
-          }
-        }
+    validate: {
+      validator: (phone) => {
+        var patt = /^[6789]\d{9}$/; return patt.test(phone)
+      }
+    }
   },
-  lastRequestTime : {type: Number, default: Date.now()},
+  lastRequestTime: { type: Number, default: Date.now() },
   name: {
-      type: String,
-      required: [true, "Name is required"],
-      minLength: 3,
-      maxLength: [40, "Exceeded Characters"]
+    type: String,
+    required: [true, "Name is required"],
+    minLength: 3,
+    maxLength: [40, "Exceeded Characters"]
   },
   yearOfBirth: {
     type: Number,
     required: true,
     minLength: 4,
     maxLength: 4,
-    validate:{
-          validator: (yearOfBirth)=>{
-            var date = new Date();
-            var year = date.getFullYear();
-            return (year >= year - 100 && year <= year + 15)
-          }
-        }
-},
- defaultAddress: {
-       addressLine:{
-         type:String
-       },
-       city:{
-         type:String
-       },
-       area:{
-         type:String
-       }
-},
- location: {
-		type: {type: String, default: "Point"},
-		coordinates: [Number]
- },
- OTP: OTPSchema
+    validate: {
+      validator: (yearOfBirth) => {
+        var date = new Date();
+        var year = date.getFullYear();
+        return (year >= year - 100 && year <= year + 15)
+      }
+    }
+  },
+  defaultAddress: {
+    addressLine: {
+      type: String
+    },
+    city: {
+      type: String
+    },
+    area: {
+      type: String
+    }
+  },
+  location: {
+    type: { type: String, default: "Point" },
+    coordinates: [Number]
+  },
+  OTP: OTPSchema
 });
 
 
