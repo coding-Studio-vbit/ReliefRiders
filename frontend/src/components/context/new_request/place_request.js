c29b34adb2e9eb220f0eed0117ea8455030e1376
Rev <rev@pop-os.localdomain>
2021-07-08 17:06:20 +0530

    place request function

---
+++ frontend/src/components/context/new_request/place_request.js
@@ -0,0 +1,30 @@
+export const placeRequest = async (formData,token,type)=>{
+
+    const imgsParsed = sessionStorage.getItem('uploaded_images')
+    const imgs = JSON.parse(imgsParsed)
+    console.log(formData);
+    for(const src in imgs){
+        console.log(imgs[src]);
+        const res = await fetch(imgs[src])
+        const blob = await res.blob()
+        const file = new File([blob],"image",{type:'image/jpg'})
+        formData.append('images',file)
+    }
+    const res = await fetch(process.env.REACT_APP_URL+`/requests/newRequest/${type==='p&d'?"pd":"general"}`,{
+        method:'POST',
+         headers: {
+             authorization: "Bearer " + token,
+           },
+         body:formData
+     })
+     const data = await res.json()
+     console.log(data);
+     if(data.status === 'success'){
+         sessionStorage.clear()
+         localStorage.removeItem('new_request')
+         return 1;
+     }else{
+         return data.message
+     }
+    
+}
