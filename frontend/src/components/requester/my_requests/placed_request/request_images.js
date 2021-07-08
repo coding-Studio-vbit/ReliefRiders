4810756a6a6527a06e3eb15acdac93a248becd28
Rev <rev@pop-os.localdomain>
2021-07-08 17:08:56 +0530

    Add proper links to images in placed request

--- frontend/src/components/requester/my_requests/placed_request/request_images.js
+++ frontend/src/components/requester/my_requests/placed_request/request_images.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import styles from './request_images.module.css'
+import React from "react";
+import styles from "./request_images.module.css";
 const RequestImages = ({ bills, images, items = [] }) => {
-    return (
-        <div className={styles.imagesContainer}>
-            {items.map((link) => (
-                <div className={styles.singleImage} key={link}>
-                    <img src={link} alt="items-img" />
-                    <span>Items</span>
-                </div>
-            ))}
-            {bills.map((link, index) => (
-                <div className={styles.singleImage} key={link}>
-                    <img src={link} alt="items-img" />
-                    <span>Bill #{index + 1}</span>
-                </div>
-            ))}
-            {images.map((link) => (
-                <div className={styles.singleImage} key={link}>
-                    <img src={link} alt="items-img" />
-                    <span>Rider-Selfie</span>
-                </div>
-            ))}
+  return (
+    <div className={styles.imagesContainer}>
+      {items.map((link) => {
+        console.log(process.env.REACT_APP_URL + link);
+
+        return (
+          <div className={styles.singleImage} key={link}>
+            <img src={process.env.REACT_APP_URL + link} alt="items-img" />
+            <span>Items</span>
+          </div>
+        );
+      })}
+      {bills.map((link, index) => (
+        <div className={styles.singleImage} key={link}>
+          <img src={process.env.REACT_APP_URL + link} alt="items-img" />
+          <span>Bill #{index + 1}</span>
         </div>
-    );
+      ))}
+      {images.map((link) => (
+        <div className={styles.singleImage} key={link}>
+          <img src={process.env.REACT_APP_URL + link} alt="items-img" />
+          <span>Rider-Selfie</span>
+        </div>
+      ))}
+    </div>
+  );
 };
-export default RequestImages
+export default RequestImages;
