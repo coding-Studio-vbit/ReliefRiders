1b2736460da9d534c0950ac7fa856e27f48787b2
Rev <revantark@gmail.com>
2021-07-18 12:21:02 +0530

    Add ViewRequest comp to respective route

--- frontend/src/components/rider/my_deliveries/my_delivery_routes.js
+++ frontend/src/components/rider/my_deliveries/my_delivery_routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import ViewRequest from '../common/viewRequest';
 import MyDeliveries from './MyDeliveries';
 const MyDeliveryRoutes = () => {
     return ( 
         <Switch>
-            <Route path="/my_deliveries/:requestID"/>
+            <Route path="/my_deliveries/:requestID" component={ViewRequest} />
 
             <Route path="/my_deliveries" component={MyDeliveries}/>
         </Switch>
