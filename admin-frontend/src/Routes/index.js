eae212bf1bf38f7f3fb6e41ea4297388fa60c44f
yasaswiraj <47531308+yasaswiraj@users.noreply.github.com>
2021-08-13 13:13:47 +0530

    addedd login and otp screens

--- admin-frontend/src/Routes/index.js
+++ admin-frontend/src/Routes/index.js
@@ -3,11 +3,12 @@ import Login from "../Components/Auth/Login";
 import NavigationBar from "../Components/GlobalComponents/NavigationBar";
 import AssignRequest from "../Components/AssignRequest";
 import OTP from "../Components/Auth/OTP";
+import createRequestGeneral from "../Components/CreateRequest/createRequestGeneral";
 const Routes = () => {
   return (
     <div>
       <NavigationBar />
-      <Route path="/temp" component={Login} />
+      <Route path="/temp" component={createRequestGeneral} />
       <Route path="/assignrequest" component={AssignRequest} />
       <Route path="/Login" component={Login} />
       <Route path="/OTP" component={OTP} />
