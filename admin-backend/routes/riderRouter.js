7100ec7e4b85bbe2e52e7efc65ea7315a5848cfb
Poojitha Mutteneni <54473815+poojithamutteneni@users.noreply.github.com>
2021-09-21 15:40:59 +0530

    Update riderRouter.js

--- admin-backend/routes/riderRouter.js
+++ admin-backend/routes/riderRouter.js
@@ -17,4 +17,41 @@ router.post("/assignRider", async(req, res) => {
     res.json(response);
 })
 
+router.get("/getDeliveries", async(req, res) => {
+    const response = await riderController.getDeliveries()
+        //send data back
+
+    res.json(response);
+})
+
+router.get("/searchByRequestNumber", async(req, res) => {
+    const response = await riderController.searchByRequestNumber(req.body.requestNumber)
+        //send data back
+
+    res.json(response);
+})
+
+router.get("/searchByName", async(req, res) => {
+    const response = await riderController.searchByName(req.body.name)
+        //send data back
+
+    res.json(response);
+})
+
+router.get("/getDeliveriesByRequestStatus", async(req, res) => {
+    const response = await riderController.getDeliveriesByRequestStatus()
+        //send data back
+
+    res.json(response);
+})
+
+
+router.get("/getDeliveriesByRequestNumber", async(req, res) => {
+    searchByRequestNumber,
+    const response = await riderController.searchByRequestNumber(req.body.requestNumber)
+        //send data back
+
+    res.json(response);
+})
+
 module.exports = router;
