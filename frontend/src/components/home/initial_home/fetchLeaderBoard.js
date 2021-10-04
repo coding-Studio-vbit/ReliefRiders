a4e4e1668f81a0319372716d3b8bc6c670c30d0a
RevantArk <revantark@gmail.com>
2021-10-04 17:06:21 +0530

    Catch errors in leaderboard component

--- frontend/src/components/home/initial_home/fetchLeaderBoard.js
+++ frontend/src/components/home/initial_home/fetchLeaderBoard.js
@@ -1,11 +1,15 @@
 export const fetchLeaderBoard = async () => {
-  const res = await fetch(
-    process.env.REACT_APP_URL + "/leaderboard/showLeaderboard",
-    {
-      method: "GET",
-    }
-  );
-  const data = await res.json();
-  console.log(data);
-  return { status: data.status === "success" ? 1 : -1, data: data.message };
+  try {
+    const res = await fetch(
+      process.env.REACT_APP_URL + "/leaderboard/showLeaderboard",
+      {
+        method: "GET",
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    return { status: data.status === "success" ? 1 : -1, data: data.message };
+  } catch (error) {
+    return {error:"Unable to access server"}
+  }
 };
