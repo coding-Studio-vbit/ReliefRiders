b9e74adc980d199e6db8dbaddbaa5fe984651d12
Prashanith <prashanith007@gmail.com>
2022-01-22 14:21:12 +0530

    Feedback form update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
-// import { AuthProvider } from "./components/context/auth/authProvider";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import InitialHomeRouting from "./components/home/initial_home/initialHomeRouting";
-// import About from "./components/about/about";
-import FeedbackForm from "./components/requester/feedback/feedbackForm";
+import { AuthProvider } from "./components/context/auth/authProvider";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import InitialHomeRouting from "./components/home/initial_home/initialHomeRouting";
+import About from "./components/about/about";
 
 function App() {
   return (
     <div className="App">
-      {/* <AuthProvider>
+      <AuthProvider>
         <Router>
           <Switch>
             <Route path="/about">
@@ -20,8 +19,7 @@ function App() {
             </Route>
           </Switch>
         </Router>
-      </AuthProvider> */}
-      <FeedbackForm/>
+      </AuthProvider>
     </div>
   );
 }
--- frontend/src/components/requester/feedback/feedbackForm.js
+++ frontend/src/components/requester/feedback/feedbackForm.js
@@ -1,17 +1,20 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useContext} from 'react';
 import Navbar from './../../global_ui/nav'
 import './feedBackForm.css'
 import TextArea from './../../global_ui/textarea/textArea'
 
 import Button from './../../global_ui/buttons/button'
-import {Spinner} from './../../global_ui/spinner';
+import {LoadingScreen, Spinner} from './../../global_ui/spinner';
 import {Dialog} from './../../global_ui/dialog/dialog'
-
+import { AuthContext } from '../../context/auth/authProvider';
+import axios from 'axios';
 
 function FeedbackForm() {
   const [pageLoad, setPageLoad] = useState(true);  
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState("");
+  const { token } = useContext(AuthContext);
 
   const [questions,setQuestions] = useState({
       ques:[
@@ -21,7 +24,7 @@ function FeedbackForm() {
           {id:4,question:"Was the application accessible to use ?",value:true},
           {id:5,question:"Are you satisfied with the service ?",value:true},    
       ],
-      query:"frnrnrnj"
+      query:""
   });
 
   async function submitFeedBackForm() {
@@ -38,95 +41,111 @@ function FeedbackForm() {
   async function getQuestions() {
     const options = {
         headers: {
-        authorization: "Bearer " + token,
+            authorization: "Bearer " + token,
         },
     };
-    axios.get(process.env.REACT_APP_URL + "/feedback", options).then(
+    axios.get(process.env.REACT_APP_URL + "/questionsList", options)
+    .then(
         (response) => {
-        console.log(response);
-        if (response.data.status === "success") {
-            setData({
-            ...response.data.message,
-            });
-
-            setError(null);
-        } else {
-            setError(response.data.message);
-        }
-        setisLoaded(true);
+            if(response.data.status==="success"){
+                if(response.data.message!=null){
+                    setError(null);
+                    //set questions
+                }else{
+                    setError("Failed to Fetch Feedback Form")
+                    console.log("Failed to Fetch Feedback Form")
+                    
+                }
+            }else{
+                setError("Failed to Fetch Feedback Form")
+                console.log("Failed to Fetch Feedback Form")
+            }
+            setPageLoad(false)          
         },
         (error) => {
-        console.error("An error occured", error);
-        setError(error.toString());
-        setisLoaded(true);
+            setError(error);
+            setPageLoad(false)
+            console.log(error);
         }
-    );        
+    ).catch(()=>{
+        setError(error);
+        setPageLoad(false)
+        console.log(error);
+    })        
   }
   
   useEffect(() => {
       getQuestions();
   }, []);
   
-  
   return(
-    <div>
-        <Navbar title='Feedback'/>
-        {
-            <Dialog isShowing={response.length>0} title="Alert" msg={response} onOK={()=>{}} />
-        }
-        <div className='feedBackForm'>
+      !pageLoad?
+        <div>
+            <Navbar title='Feedback'/>
             {
-                questions.ques.map((q)=>{
-                    return <div key={q.id}  className='radioButton' >                
-                                <label>
-                                    {q.question}
-                                </label>
-                                <span >
-                                    <input type="radio" value={true} name={q.id}
-                                            checked={q.value === true}
-                                            onChange={
-                                                function() {
-                                                    let updatedQues = questions.ques;
-                                                    const uQ = (element) => element.id==q.id;
-                                                    const i = updatedQues.findIndex(uQ)
-                                                    updatedQues[i]={id:q.id,question:q.question,value:true},
-                                                    setQuestions({...questions,ques:updatedQues})   
-                                                }                                                
-                                            }
-                                        />Yes
-                                    <input type="radio" value={false} name={q.id} 
-                                        checked={q.value === false}
-                                        onChange={
-                                            function() {
-                                                let updatedQues = questions.ques;
-                                                const uQ = (element) => element.id==q.id;
-                                                const i = updatedQues.findIndex(uQ)
-                                                updatedQues[i]={id:q.id,question:q.question,value:false},
-                                                setQuestions({...questions,ques:updatedQues})   
-                                            }                                                
-                                        }
-                                    /> No
-                                </span>             
-                            </div> 
-                })
-            }    
-            <div style={{marginTop:'40px'}}>
-            <label >Any other things you want to mention ?</label>
-            <TextArea value={questions.query} onChange={(e)=>setQuestions({...questions,query:e.target.value})}/>
-            </div>
-           
-            <div style={{width:'200px',margin:'35px auto'}}>
-                {
-                    loading?
-                    <Spinner radius={2}/>:
-                    <Button isBlock={true} isRounded={true} text={'SUBMIT'} 
-                        paddingTB={"20px"} fontSize={"20px"} marginTB={"20px"} 
-                        onClick={()=>submitFeedBackForm()}/>
-                }
-            </div>
-        </div>             
-    </div>
-  );
+                <Dialog isShowing={response.length>0} title="Alert" msg={response} onOK={()=>{}} />
+            }
+            {
+                error==null?
+                <div className='feedBackForm'>
+                    {
+                        questions.ques.map((q)=>{
+                            return <div key={q.id}  className='radioButton' >                
+                                        <label>
+                                            {q.question}
+                                        </label>
+                                        <span >
+                                            <input type="radio" value={true} name={q.id}
+                                                    checked={q.value === true}
+                                                    onChange={
+                                                        function() {
+                                                            let updatedQues = questions.ques;
+                                                            const uQ = (element) => element.id==q.id;
+                                                            const i = updatedQues.findIndex(uQ)
+                                                            updatedQues[i]={id:q.id,question:q.question,value:true},
+                                                            setQuestions({...questions,ques:updatedQues})   
+                                                        }                                                
+                                                    }
+                                                />Yes
+                                            <input type="radio" value={false} name={q.id} 
+                                                checked={q.value === false}
+                                                onChange={
+                                                    function() {
+                                                        let updatedQues = questions.ques;
+                                                        const uQ = (element) => element.id==q.id;
+                                                        const i = updatedQues.findIndex(uQ)
+                                                        updatedQues[i]={id:q.id,question:q.question,value:false},
+                                                        setQuestions({...questions,ques:updatedQues})   
+                                                    }                                                
+                                                }
+                                            /> No
+                                        </span>             
+                                    </div> 
+                        })
+                    }    
+                    <div style={{marginTop:'40px'}}>
+                        <label >Any other things you want to mention ?</label>
+                        <TextArea value={questions.query} onChange={(e)=>setQuestions({...questions,query:e.target.value})}/>
+                    </div>
+                
+                    <div style={{width:'200px',margin:'35px auto'}}>
+                        {
+                            loading?
+                            <Spinner radius={2}/>:
+                            <Button isBlock={true} isRounded={true} text={'SUBMIT'} 
+                                paddingTB={"20px"} fontSize={"20px"} marginTB={"20px"} 
+                                onClick={()=>submitFeedBackForm()}/>
+                        }
+                    </div>
+                </div>  
+                :<div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                    {error}
+                </div>          
+            }
+             
+        </div>:
+        <LoadingScreen/>
+    );
 }
 
 export default FeedbackForm;
