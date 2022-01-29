9abc8d63915d9e3e11232d0b1068851891ba988c
RevantArk <revantark@gmail.com>
2022-01-29 16:00:07 +0530

    Add token auth

--- backend/routes/feedback/router.js
+++ backend/routes/feedback/router.js
@@ -1,8 +1,9 @@
 
 const express = require("express");
 const { saveFeedback } = require("../../controllers/feedbackController");
+const tokenAuth = require("../common/tokenAuth");
 const router = express.Router();
-
+router.use(tokenAuth)
 router.post("/", saveFeedback)
 
 module.exports = router;
